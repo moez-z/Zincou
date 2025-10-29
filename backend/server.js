97265617a0c0eab2c86cc361558b7d42599a3f88
moez-z <68192571+moez-z@users.noreply.github.com>
2025-10-29 11:38:00 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 // CORS configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://zincou-frontend.onrender.com",
     credentials: true, // Allow cookies
   })
 );
